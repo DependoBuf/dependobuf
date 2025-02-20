a70b8803bbe157a05610b1f6dc5cdba789785f02
Ilya Tkemaladze <intkemaladze@gmail.com>
2025-02-20 23:28:41 +0300

    add AstAccess

--- dbuf-lsp/src/main.rs
+++ dbuf-lsp/src/main.rs
@@ -1,22 +1,31 @@
+mod ast_access;
+
 use tower_lsp::jsonrpc::Result;
 use tower_lsp::lsp_types::*;
 use tower_lsp::{Client, LanguageServer, LspService, Server};
 
-#[allow(unused_imports)]
-#[allow(clippy::single_component_path_imports)]
-use dbuf_core;
+use ast_access::AstAccess;
 
 #[derive(Debug)]
-#[allow(dead_code)]
 struct Backend {
     client: Client,
 
-    ast: Option<()>,
+    ast: AstAccess,
+}
+
+impl Backend {
+    fn new(client: Client) -> Backend {
+        Backend {
+            client,
+            ast: AstAccess::new(),
+        }
+    }
 }
 
 #[tower_lsp::async_trait]
 impl LanguageServer for Backend {
     async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+        let _ast = self.ast.write();
         eprintln!("init");
         Ok(InitializeResult {
             capabilities: ServerCapabilities {
@@ -46,6 +55,7 @@ impl LanguageServer for Backend {
         // eprintln!("did open: {:?}", params);
         // TODO: read params.text_document.text, containing full document text and build AST
         let _ = params;
+        let _ast = self.ast.write();
         eprintln!("WARN: did open is not fully implemented")
     }
 
@@ -53,18 +63,21 @@ impl LanguageServer for Backend {
         // eprintln!("did change: {:?}", params);
         // TODO: read params.content_changes[0].text, containing full document text and build AST
         let _ = params;
+        let _ast = self.ast.write();
         eprintln!("WARN: did change is not fully implemented")
     }
     async fn did_close(&self, params: DidCloseTextDocumentParams) {
         // eprintln!("did close: {:?}", params);
         // TODO: remove existing AST
         let _ = params;
+        let _ast = self.ast.write();
         eprintln!("WARN: did close is not fully implemented");
     }
 
     async fn completion(&self, params: CompletionParams) -> Result<Option<CompletionResponse>> {
         eprintln!("WARN: completition is not fully implemented");
         let _ = params;
+        let _ast = self.ast.read();
         Ok(Some(CompletionResponse::Array(vec![
             CompletionItem {
                 label: "message".to_string(),
@@ -86,6 +99,7 @@ impl LanguageServer for Backend {
     async fn hover(&self, params: HoverParams) -> Result<Option<Hover>> {
         eprintln!("WARN: hover is not fully implemented");
         let _ = params;
+        let _ast = self.ast.read();
         Ok(Some(Hover {
             contents: HoverContents::Scalar(MarkedString::String("You're hovering!".to_string())),
             range: None,
@@ -98,6 +112,6 @@ async fn main() {
     let stdin = tokio::io::stdin();
     let stdout = tokio::io::stdout();
 
-    let (service, socket) = LspService::new(|client| Backend { client, ast: None });
+    let (service, socket) = LspService::new(Backend::new);
     Server::new(stdin, stdout, socket).serve(service).await;
 }
