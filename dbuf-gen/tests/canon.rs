d24337837d59152ee8457b12c52b786ba384ad63
Shlyakhtun Prokhor <shlyakhtun.prohor@gmail.com>
2025-06-28 10:46:07 +0300

    Minor improvements
    Moved test data into a separate file
    Fixed a typo

--- dbuf-gen/tests/canon.rs
+++ dbuf-gen/tests/canon.rs
@@ -1,60 +1,20 @@
+mod common;
+
+
 mod tests {
     use dbuf_core::ast::elaborated as e;
     use dbuf_gen::codegen;
     use pretty_assertions::assert_eq;
-
+    use crate::common;
+    
     #[test]
     fn basic() {
-        let module = e::Module {
-            types: vec![(
-                "Nat".to_owned(),
-                e::Type {
-                    dependencies: Vec::new(),
-                    constructor_names: e::ConstructorNames::OfEnum(
-                        ["Zero", "Suc"].into_iter().map(|s| s.to_owned()).collect(),
-                    ),
-                },
-            )],
-            constructors: vec![
-                (
-                    "Zero".to_owned(),
-                    e::Constructor {
-                        implicits: Vec::new(),
-                        fields: Vec::new(),
-                        result_type: e::TypeExpression::TypeExpression {
-                            name: "Nat".to_owned(),
-                            dependencies: e::Rec::new([]),
-                        },
-                    },
-                ),
-                (
-                    "Suc".to_owned(),
-                    e::Constructor {
-                        implicits: Vec::new(),
-                        fields: vec![(
-                            "pred".to_owned(),
-                            e::TypeExpression::TypeExpression {
-                                name: "Nat".to_owned(),
-                                dependencies: e::Rec::new([]),
-                            },
-                        )],
-                        result_type: e::TypeExpression::TypeExpression {
-                            name: "Nat".to_owned(),
-                            dependencies: e::Rec::new([]),
-                        },
-                    },
-                ),
-            ]
-            .into_iter()
-            .collect(),
-        };
-
+        let module = common::get_basic_module();
         let mut writer = Vec::new();
+
         assert!(codegen::generate_module(module, &mut writer).is_ok());
 
         let code = String::from_utf8(writer).expect("generated code must be correct utf8");
-        // println!("{}", code);
-
         let expected = include_str!("./canon/basic.rs");
 
         assert_eq!(code, expected);
@@ -62,144 +22,12 @@ mod tests {
 
     #[test]
     fn nat_vec() {
-        let module = e::Module {
-            types: vec![
-                (
-                    "Nat".to_owned(),
-                    e::Type {
-                        dependencies: Vec::new(),
-                        constructor_names: e::ConstructorNames::OfEnum(
-                            ["Zero", "Suc"].into_iter().map(|s| s.to_owned()).collect(),
-                        ),
-                    },
-                ),
-                (
-                    "Vec".to_owned(),
-                    e::Type {
-                        dependencies: vec![(
-                            "n".to_owned(),
-                            e::TypeExpression::TypeExpression {
-                                name: "Nat".to_owned(),
-                                dependencies: e::Rec::new([]),
-                            },
-                        )],
-                        constructor_names: e::ConstructorNames::OfEnum(
-                            ["Nil", "Cons"].into_iter().map(|s| s.to_owned()).collect(),
-                        ),
-                    },
-                ),
-            ],
-            constructors: vec![
-                (
-                    "Zero".to_owned(),
-                    e::Constructor {
-                        implicits: Vec::new(),
-                        fields: Vec::new(),
-                        result_type: e::TypeExpression::TypeExpression {
-                            name: "Nat".to_owned(),
-                            dependencies: e::Rec::new([]),
-                        },
-                    },
-                ),
-                (
-                    "Suc".to_owned(),
-                    e::Constructor {
-                        implicits: Vec::new(),
-                        fields: vec![(
-                            "pred".to_owned(),
-                            e::TypeExpression::TypeExpression {
-                                name: "Nat".to_owned(),
-                                dependencies: e::Rec::new([]),
-                            },
-                        )],
-                        result_type: e::TypeExpression::TypeExpression {
-                            name: "Nat".to_owned(),
-                            dependencies: e::Rec::new([]),
-                        },
-                    },
-                ),
-                (
-                    "Nil".to_owned(),
-                    e::Constructor {
-                        implicits: Vec::new(),
-                        fields: Vec::new(),
-                        result_type: e::TypeExpression::TypeExpression {
-                            name: "Vec".to_owned(),
-                            dependencies: e::Rec::new([e::ValueExpression::Constructor {
-                                name: "Zero".to_owned(),
-                                implicits: e::Rec::new([]),
-                                arguments: e::Rec::new([]),
-                                result_type: e::TypeExpression::TypeExpression {
-                                    name: "Nat".to_owned(),
-                                    dependencies: e::Rec::new([]),
-                                },
-                            }]),
-                        },
-                    },
-                ),
-                (
-                    "Cons".to_owned(),
-                    e::Constructor {
-                        implicits: vec![(
-                            "p".to_owned(),
-                            e::TypeExpression::TypeExpression {
-                                name: "Nat".to_owned(),
-                                dependencies: e::Rec::new([]),
-                            },
-                        )],
-                        fields: vec![
-                            (
-                                "val".to_owned(),
-                                e::TypeExpression::TypeExpression {
-                                    name: "Nat".to_owned(),
-                                    dependencies: e::Rec::new([]),
-                                },
-                            ),
-                            (
-                                "tail".to_owned(),
-                                e::TypeExpression::TypeExpression {
-                                    name: "Vec".to_owned(),
-                                    dependencies: e::Rec::new([e::ValueExpression::Variable {
-                                        name: "p".to_owned(),
-                                        ty: e::TypeExpression::TypeExpression {
-                                            name: "Nat".to_owned(),
-                                            dependencies: e::Rec::new([]),
-                                        },
-                                    }]),
-                                },
-                            ),
-                        ],
-                        result_type: e::TypeExpression::TypeExpression {
-                            name: "Vec".to_owned(),
-                            dependencies: e::Rec::new([e::ValueExpression::Constructor {
-                                name: "Suc".to_owned(),
-                                implicits: e::Rec::new([]),
-                                arguments: e::Rec::new([e::ValueExpression::Variable {
-                                    name: "p".to_owned(),
-                                    ty: e::TypeExpression::TypeExpression {
-                                        name: "Nat".to_owned(),
-                                        dependencies: e::Rec::new([]),
-                                    },
-                                }]),
-                                result_type: e::TypeExpression::TypeExpression {
-                                    name: "Nat".to_owned(),
-                                    dependencies: e::Rec::new([]),
-                                },
-                            }]),
-                        },
-                    },
-                ),
-            ]
-            .into_iter()
-            .collect(),
-        };
-
+        let module = common::get_nat_vec_module();
         let mut writer = Vec::new();
+
         assert!(codegen::generate_module(module, &mut writer).is_ok());
 
         let code = String::from_utf8(writer).expect("generated code must be correct utf8");
-        // println!("{}", code);
-
         let expected = include_str!("./canon/nat_vec.rs");
 
         assert_eq!(code, expected);
