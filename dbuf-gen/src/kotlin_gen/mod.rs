7d20d28d5af036663987f2170e3aa849c1f7f9fc
Shlyakhtun Prokhor <shlyakhtun.prohor@gmail.com>
2025-07-01 23:49:05 +0300

    Moved language primitives into a separate file

--- dbuf-gen/src/kotlin_gen/mod.rs
+++ dbuf-gen/src/kotlin_gen/mod.rs
@@ -1,6 +1,8 @@
 use crate::ast;
 
 mod generate;
+mod target;
+
 
 pub fn generate_module(module: ast::elaborated::Module<String>) -> String {
     let module = ast::Module::from_elaborated(module);
