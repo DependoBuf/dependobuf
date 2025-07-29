ed65ec0c748ba7d87cc329ea9c66e139b2b5a37b
Илья Ткемаладзе <intkemaladze@gmail.com>
2025-07-30 02:39:16 +0300

    add located string

---
+++ dbuf-core/src/ast/parsed/located_string.rs
@@ -0,0 +1,22 @@
+//! Strings for parsed AST.
+
+use std::ops::{Add, Deref};
+
+/// Single line string with location.
+pub struct LocatedString<String, Pos> {
+    /// String content.
+    pub content: String,
+    /// Starting position of string.
+    pub start: Pos,
+}
+
+impl<String, Pos> LocatedString<String, Pos>
+where
+    String: Deref<Target = [u8]>,
+    Pos: Copy + Add<usize, Output = Pos>,
+{
+    /// Ending position of a string.
+    pub fn end(&self) -> Pos {
+        self.start + self.content.len()
+    }
+}
