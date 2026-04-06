dfc5addc3d81b249c376ccbcbe0a33818d433aff
Ilya Tkemaladze <intkemaladze@gmail.com>
2026-04-06 21:12:16 +0300

    some tests fixes

--- dbuf-core/src/cst/parser.rs
+++ dbuf-core/src/cst/parser.rs
@@ -13,6 +13,7 @@ use crate::location::Offset;
 
 use super::parser_error::ParsingError;
 use super::parser_utils::{MapChild, MapToken, MapTree};
+use super::parser_whitespace::WhiteSpace;
 
 /// State of parsing used for emitting correct locations of errors.
 #[derive(Clone, Default)]
@@ -65,11 +66,11 @@ where
 {
     let message = message_parser().map_child().map(|c| vec![c]);
     let enum_parser = enum_parser().map_child().map(|c| vec![c]);
-    let comment = WhiteSpaceConfig::new()
+    let comment = WhiteSpace::new()
         .with_limit_comment()
         .with_progress()
         .parser();
-    let ws = WhiteSpaceConfig::new().with_progress().parser();
+    let ws = WhiteSpace::new().with_no_comment().with_progress().parser();
 
     let recovery_on = comment
         .clone()
@@ -87,7 +88,7 @@ where
         .map_child()
         .map(|c| vec![c]);
 
-    let one_block = choice((message, enum_parser, comment, ws)).recover_with(via_parser(recovery));
+    let one_block = choice((ws, message, enum_parser, comment)).recover_with(via_parser(recovery));
 
     one_block
         .repeated()
@@ -107,16 +108,16 @@ fn message_parser<'src, I>() -> impl Parser<'src, I, Tree, ExtraData> + Clone
 where
     I: ValueInput<'src, Span = Location<Offset>, Token = Token>,
 {
-    let one_comment = WhiteSpaceConfig::new().with_limit_comment().parser();
-    let ws = WhiteSpaceConfig::new().parser();
+    let bind_comment = WhiteSpace::new().with_bind_comment().parser().or_not();
+    let ws = WhiteSpace::new().parser();
+
     let message_kw = just(Token::Message).map_token();
     let type_ident = type_identifier_parser();
     let dependency = dependency_parser().then(ws.clone());
     let dependency_r = dependency.repeated().collect::<Vec<_>>();
     let body = body_parser();
 
-    one_comment
-        .or_not()
+    bind_comment
         .then(message_kw)
         .then(ws.clone())
         .then(type_ident)
@@ -136,7 +137,7 @@ fn dependency_parser<'src, I>() -> impl Parser<'src, I, Tree, ExtraData> + Clone
 where
     I: ValueInput<'src, Span = Location<Offset>, Token = Token>,
 {
-    let ws = WhiteSpaceConfig::new().parser();
+    let ws = WhiteSpace::new().parser();
 
     let lparen = just(Token::LParen).map_token();
     let definition = definition_parser();
@@ -172,7 +173,8 @@ fn body_parser<'src, I>() -> impl Parser<'src, I, Tree, ExtraData> + Clone
 where
     I: ValueInput<'src, Span = Location<Offset>, Token = Token>,
 {
-    let ws = WhiteSpaceConfig::new()
+    let ws = WhiteSpace::new().with_no_comment().with_progress().parser();
+    let comment = WhiteSpace::new()
         .with_limit_comment()
         .with_progress()
         .parser();
@@ -181,7 +183,7 @@ where
     let rbrace = just(Token::RBrace).map_token();
 
     let field = field_parser().map_child().map(|c| vec![c]);
-    let inside = choice((field, ws))
+    let inside = choice((ws, field, comment))
         .repeated()
         .collect::<Vec<_>>()
         .map(Vec::flatten);
@@ -205,8 +207,8 @@ fn field_parser<'src, I>() -> impl Parser<'src, I, Tree, ExtraData> + Clone
 where
     I: ValueInput<'src, Span = Location<Offset>, Token = Token>,
 {
-    let one_comment = WhiteSpaceConfig::new().with_limit_comment().parser();
-    let ws = WhiteSpaceConfig::new().parser();
+    let bind_comment = WhiteSpace::new().with_bind_comment().parser().or_not();
+    let ws = WhiteSpace::new().with_no_new_line().parser();
 
     let definition = definition_parser();
 
@@ -220,8 +222,7 @@ where
     });
     let maybe_semicolon = semicolon.or(no_semicolon);
 
-    one_comment
-        .or_not()
+    bind_comment
         .then(definition.map_with(|d, extra| {
             let l = extra.span();
             extra.state().definition_start = Some(l);
@@ -254,7 +255,7 @@ fn definition_parser<'src, I>() -> impl Parser<'src, I, Tree, ExtraData> + Clone
 where
     I: ValueInput<'src, Span = Location<Offset>, Token = Token>,
 {
-    let ws = WhiteSpaceConfig::new().with_no_new_line().parser();
+    let ws = WhiteSpace::new().with_no_new_line().parser();
 
     let var_ident = var_identifier_parser();
     let type_ident = type_identifier_parser();
@@ -298,7 +299,7 @@ fn constructed_value_parser_impl<'src, I>(
 where
     I: ValueInput<'src, Span = Location<Offset>, Token = Token>,
 {
-    let ws = WhiteSpaceConfig::new().parser();
+    let ws = WhiteSpace::new().parser();
 
     let type_indent = type_identifier_parser();
     let lbrace = just(Token::LBrace).map_token();
@@ -317,18 +318,32 @@ where
         .then(colon)
         .then(ws.clone())
         .then(field_value)
-        .then(ws.clone())
-        .then(comma.or_not())
+        .then(ws.clone());
+
+    let field_init_last = field_init
+        .clone()
         .map_tree(TreeKind::ConstructedValueField)
         .map_child();
 
-    let field_r = field_init.repeated().at_least(1).collect::<Vec<_>>();
+    let field_init_not_last = field_init
+        .then(comma)
+        .map_tree(TreeKind::ConstructedValueField)
+        .map_child();
+
+    let field_r = field_init_not_last
+        .repeated()
+        .collect::<Vec<_>>()
+        .then(field_init_last);
+
+    let inner = field_r
+        .map(|a| (Some(a), None))
+        .or(ws.clone().map(|a| (None, Some(a))));
 
     type_indent
         .then(ws.clone())
         .then(lbrace)
-        .then(choice((field_r, ws.clone())))
-        .then(ws.clone())
+        .then(inner)
+        .then(ws)
         .then(rbrace)
         .map_tree(TreeKind::ConstructedValue)
 }
@@ -372,7 +387,7 @@ fn expression_parser_impl<'src, I>(
 where
     I: ValueInput<'src, Span = Location<Offset>, Token = Token>,
 {
-    let ws = WhiteSpaceConfig::new().with_no_new_line().parser();
+    let ws = WhiteSpace::new().with_no_new_line().parser();
 
     let literal_atom = literal_parser().map_tree(TreeKind::ExprLiteral);
     let identifier_atom = var_chain_parser();
@@ -475,8 +490,8 @@ fn enum_parser<'src, I>() -> impl Parser<'src, I, Tree, ExtraData> + Clone
 where
     I: ValueInput<'src, Span = Location<Offset>, Token = Token>,
 {
-    let one_comment = WhiteSpaceConfig::new().with_limit_comment().parser();
-    let ws = WhiteSpaceConfig::new().parser();
+    let bind_comment = WhiteSpace::new().with_bind_comment().parser().or_not();
+    let ws = WhiteSpace::new().parser();
 
     let enum_kw = just(Token::Enum).map_token();
     let type_ident = type_identifier_parser();
@@ -484,8 +499,7 @@ where
     let dependency_r = dependency.repeated().collect::<Vec<_>>();
     let enum_body = enum_body_parser();
 
-    one_comment
-        .or_not()
+    bind_comment
         .then(enum_kw)
         .then(ws.clone())
         .then(type_ident)
@@ -511,7 +525,8 @@ fn enum_body_parser<'src, I>() -> impl Parser<'src, I, Tree, ExtraData> + Clone
 where
     I: ValueInput<'src, Span = Location<Offset>, Token = Token>,
 {
-    let ws = WhiteSpaceConfig::new()
+    let ws = WhiteSpace::new().with_no_comment().with_progress().parser();
+    let comment = WhiteSpace::new()
         .with_limit_comment()
         .with_progress()
         .parser();
@@ -520,7 +535,7 @@ where
     let rbrace = just(Token::RBrace).map_token();
 
     let branch = branch_parser().map_child().map(|c| vec![c]);
-    let inside = choice((branch, ws))
+    let inside = choice((ws, branch, comment))
         .repeated()
         .collect::<Vec<_>>()
         .map(Vec::flatten);
@@ -546,7 +561,7 @@ fn branch_parser<'src, I>() -> impl Parser<'src, I, Tree, ExtraData> + Clone
 where
     I: ValueInput<'src, Span = Location<Offset>, Token = Token>,
 {
-    let ws = WhiteSpaceConfig::new().parser();
+    let ws = WhiteSpace::new().parser();
 
     let pattern = pattern_parser();
     let comma = just(Token::Comma).map_token();
@@ -590,7 +605,7 @@ fn pattern_parser_impl<'src, I>(
 where
     I: ValueInput<'src, Span = Location<Offset>, Token = Token>,
 {
-    let ws = WhiteSpaceConfig::new().parser();
+    let ws = WhiteSpace::new().parser();
 
     let star = just(Token::Star).map_token();
     let literal = literal_parser();
@@ -609,18 +624,31 @@ where
         .then(colon)
         .then(ws.clone())
         .then(p_parser)
-        .then(ws.clone())
-        .then(comma.or_not())
-        .then(ws.clone())
+        .then(ws.clone());
+
+    let field_init_last = field_init
+        .clone()
         .map_tree(TreeKind::ConstructedPatternField)
         .map_child();
 
-    let field_r = field_init.repeated().at_least(1).collect::<Vec<_>>();
+    let field_init_not_last = field_init
+        .then(comma)
+        .map_tree(TreeKind::ConstructedPatternField)
+        .map_child();
+
+    let field_r = field_init_not_last
+        .repeated()
+        .collect::<Vec<_>>()
+        .then(field_init_last);
+
+    let inner = field_r
+        .map(|a| (Some(a), None))
+        .or(ws.clone().map(|a| (None, Some(a))));
 
     let constructed_pattern = type_indent
-        .then(ws.clone())
+        .then(ws)
         .then(lbrace)
-        .then(choice((field_r, ws)))
+        .then(inner)
         .then(rbrace)
         .map_tree(TreeKind::ConstructedPattern)
         .map_child();
@@ -640,7 +668,8 @@ fn constructor_enum_parser<'src, I>() -> impl Parser<'src, I, Tree, ExtraData> +
 where
     I: ValueInput<'src, Span = Location<Offset>, Token = Token>,
 {
-    let ws = WhiteSpaceConfig::new()
+    let ws = WhiteSpace::new().with_no_comment().with_progress().parser();
+    let comment = WhiteSpace::new()
         .with_limit_comment()
         .with_progress()
         .parser();
@@ -649,7 +678,7 @@ where
     let rbrace = just(Token::RBrace).map_token();
 
     let constructor = constructor_parser().map_child().map(|c| vec![c]);
-    let inside = choice((constructor, ws))
+    let inside = choice((ws, constructor, comment))
         .repeated()
         .collect::<Vec<_>>()
         .map(Vec::flatten);
@@ -677,138 +706,25 @@ fn constructor_parser<'src, I>() -> impl Parser<'src, I, Tree, ExtraData> + Clon
 where
     I: ValueInput<'src, Span = Location<Offset>, Token = Token>,
 {
-    let one_comment = WhiteSpaceConfig::new().with_limit_comment().parser();
-    let ws = WhiteSpaceConfig::new().parser();
+    let bind_comment = WhiteSpace::new().with_bind_comment().parser().or_not();
+    let ws = WhiteSpace::new().parser();
+
     let type_indent = type_identifier_parser();
     let body = body_parser();
 
-    let constructor_with_body = one_comment
+    let constructor_with_body = bind_comment
         .clone()
         .then(type_indent.clone())
         .then(ws)
         .then(body)
         .map_tree(TreeKind::Constructor);
 
-    let constructor_no_body = one_comment
+    let constructor_no_body = bind_comment
+        .or_not()
         .then(type_indent)
         .map_tree(TreeKind::Constructor);
 
     choice((constructor_with_body, constructor_no_body))
-}
-
-/// Configuration for whitespace parser.
-#[allow(
-    clippy::struct_excessive_bools,
-    reason = "That not a state machine and all states are correct"
-)]
-struct WhiteSpaceConfig {
-    /// consume regular spaces.
-    consume_space: bool,
-    /// consume new lines.
-    consume_newline: bool,
-    /// consume block comments.
-    consume_block_comment: bool,
-    /// consume line comments.
-    consume_line_comment: bool,
-    /// consume error tokens.
-    consume_error: bool,
-    /// require at least one success token.
-    progress: bool,
-    /// consume comment tokens no more that once.
-    limit_comment: bool,
-}
-
-impl WhiteSpaceConfig {
-    fn new() -> WhiteSpaceConfig {
-        WhiteSpaceConfig {
-            consume_space: true,
-            consume_newline: true,
-            consume_block_comment: true,
-            consume_line_comment: true,
-            consume_error: true,
-            progress: false,
-            limit_comment: false,
-        }
-    }
-
-    fn with_progress(mut self) -> WhiteSpaceConfig {
-        self.progress = true;
-        self
-    }
-
-    fn with_limit_comment(mut self) -> WhiteSpaceConfig {
-        self.limit_comment = true;
-        self
-    }
-
-    fn with_no_new_line(mut self) -> WhiteSpaceConfig {
-        self.consume_newline = false;
-        self
-    }
-
-    fn parser<'src, I>(self) -> impl Parser<'src, I, Vec<Child>, ExtraData> + Clone
-    where
-        I: ValueInput<'src, Span = Location<Offset>, Token = Token>,
-    {
-        let space = just(Token::Space).map_token().labelled("Space");
-
-        let new_line = just(Token::NewLine).map_token().labelled("New Line");
-
-        let line_comment = select! {
-            Token::LineComment(comment) => Token::LineComment(comment)
-        }
-        .map_token()
-        .labelled("Line Comment");
-
-        let block_comment = select! {
-            Token::BlockComment(comment) => Token::BlockComment(comment)
-        }
-        .map_token()
-        .labelled("Block Comment");
-
-        let error = just(Token::Err).map_token().labelled("Error");
-
-        let mut comment_parser = any().filter(|_| false).map(|_| unreachable!()).boxed();
-        if self.consume_block_comment {
-            comment_parser = block_comment.or(comment_parser).boxed();
-        }
-        if self.consume_line_comment {
-            comment_parser = line_comment.or(comment_parser).boxed();
-        }
-
-        let mut ws = any().filter(|_| false).map(|_| unreachable!()).boxed();
-        if self.consume_space {
-            ws = space.or(ws).boxed();
-        }
-        if self.consume_newline {
-            ws = new_line.or(ws).boxed();
-        }
-        if self.consume_error {
-            ws = error.or(ws).boxed();
-        }
-
-        let no_lim = choice((comment_parser.clone(), ws.clone()))
-            .repeated()
-            .collect::<Vec<_>>();
-        let limited = ws
-            .clone()
-            .repeated()
-            .collect::<Vec<_>>()
-            .then(comment_parser.or_not())
-            .then(ws.repeated().collect::<Vec<_>>())
-            .map(|((mut ws1, comm), ws2)| {
-                ws1.extend(comm);
-                ws1.extend(ws2);
-                ws1
-            });
-
-        match (self.progress, self.limit_comment) {
-            (true, true) => limited.filter(|v| !v.is_empty()).boxed(),
-            (true, false) => no_lim.filter(|v| !v.is_empty()).boxed(),
-            (false, true) => limited.boxed(),
-            (false, false) => no_lim.boxed(),
-        }
-    }
 }
 
 /// Parses type identifier (`UCIdentifier`).
